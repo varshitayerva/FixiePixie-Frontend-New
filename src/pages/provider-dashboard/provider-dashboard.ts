fde0053f06728a4db0efc557cb0b5b605df06ef2
Pankaj <l.venkat@globallogic.com>
2026-04-10 15:05:12 +0530

    Added files for provider-dashboard

---
+++ src/pages/provider-dashboard/provider-dashboard.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { BookService } from '../book-service/book-service';
+import { Navbar } from "../../navbar/navbar";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-provider-dashboard',
+  imports: [MatCardModule, MatButtonModule, RouterModule,Navbar],
+  templateUrl: './provider-dashboard.html',
+  styleUrl: './provider-dashboard.css',
+})
+
+
+export class ProviderDashboard {
+  constructor(
+    private router: Router,
+    private cookieService: CookieService
+  ) { }
+  
+}
